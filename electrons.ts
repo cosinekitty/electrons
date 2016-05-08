104199b417320b0e0e16d50901daef3c09364d8a
Don Cross <cosinekitty@gmail.com>
2016-05-08 16:10:35 -0400

    Starting new TypeScript project for simulating electrons on the surface of a sphere.

---
+++ electrons.ts
@@ -0,0 +1,12 @@
+/*
+    electrons.ts  -  Don Cross  -  http://cosinekitty.com
+*/
+
+/// <reference path="jquery.d.ts" />
+
+module Electrons {
+
+    $(document).ready(function(){
+        console.log('Electrons on a Sphere - ready.');
+    });
+}
